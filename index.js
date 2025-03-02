6f7fa3e1221845d98d632cc1d1f724fae048df76
phsoftware-corp <heckervirusads+free@gmail.com>
2025-03-02 02:22:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log(location.href + " is loading...")
-console.log("Site successfully loaded in: " + Math.random(1) + " miliseconds."
+console.log("Site successfully loaded in: " + Math.random(1) + " miliseconds.")
